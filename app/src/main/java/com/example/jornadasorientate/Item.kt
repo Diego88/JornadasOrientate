ec2d00b564bc967d9a9a50185408294695dd6efe
Diego Moya <dmoya@hiberus.com>
2022-02-14 09:21:07 +0100

    Add recyclerview and adapter

---
+++ app/src/main/java/com/example/jornadasorientate/Item.kt
@@ -0,0 +1,8 @@
+package com.example.jornadasorientate
+
+data class Item(
+    val id: Int,
+    val title: String?,
+    val description: String?,
+    val userId: Int
+)
---
+++ app/src/main/java/com/example/jornadasorientate/Util.kt
@@ -0,0 +1,21 @@
+package com.example.jornadasorientate
+
+fun getItemList(): List<Item> {
+    return listOf(
+        Item(
+            1, "Title 1", "Description 1", 1
+        ),
+        Item(
+            2, "Title 2", "Description 2", 2
+        ),
+        Item(
+            3, "Title 3", "Description 3", 3
+        ),
+        Item(
+            4, "Title 4", "Description 4", 4
+        ),
+        Item(
+            5, "Title 5", "Description 5", 5
+        ),
+    )
+}
